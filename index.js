4e94ce8375c2299f3cdba56706824150fff2a257
蓝方-Blusque <2635857879@qq.com>
2022-10-20 09:39:24 +0800

    10/20 commit 1st

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const market = require("./database/market")
+const fs = require('fs')
 const mysql = require("mysql");
 
 const app = express();
@@ -9,18 +9,26 @@ var router = express.Router();
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 
-const marketConn = mysql.createConnection(market.market);
+try {
+    var context = fs.readFileSync('./database/market.json', 'utf8');
+    var market = JSON.parse(context);
+} catch (e) {
+    console.log(e);
+    process.exit(1);
+}
+const marketConn = mysql.createConnection(market);
 
 marketConn.connect();
 
 app.get("/get", (req, res) => {
-    marketConn.query("select * from user", (error, result) => {
+    marketConn.query("select * from customer", (error, result) => {
         if (error) throw error;
         res.send({
             message: "success",
             status: 200,
             data: result,
         });
+        console.log("get success!");
     });
 });
 
